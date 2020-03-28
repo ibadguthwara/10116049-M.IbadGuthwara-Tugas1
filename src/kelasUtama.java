d2b34ac3ee1403f26814fb5d5dc6e38d1d7b2835
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2020-03-28 21:28:04 +0700

    pertama unggah file

---
+++ src/kelasUtama.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ibadguthwara
+ * 10116049
+ * IF-9
+ */
+public class kelasUtama {
+    public static void main(String[] args){
+        fUtama frame = new fUtama();
+        frame.setVisible(true);
+    }
+    
+}
